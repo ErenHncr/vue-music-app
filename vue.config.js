5872b10d06ad69ef1438b0182789cea8fb34469d
erenhncr <erenhncr@gmail.com>
2021-06-21 20:28:46 +0300

    vue.config added

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+// vue.config.js
+module.exports = {
+  // this line added for fixing blank page in index.html
+  publicPath: process.env.NODE_ENV === 'production'
+    ? './'
+    : '/',
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: ``,
+        // @import "@/assets/scss/app.scss";
+      },
+    }
+  },
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = "Music App";
+        return args;
+      })
+  }
+}
