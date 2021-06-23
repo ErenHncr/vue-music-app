807bddbbf805355d392d00b1add14b66764a3c71
erenhncr <erenhncr@gmail.com>
2021-06-23 19:12:42 +0300

    new actions added

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex';
+import { auth, usersCollection } from '@/includes/firebase';
 
 export default createStore({
   state: {
@@ -11,6 +12,41 @@ export default createStore({
     },
     toggleAuth: (state) => {
       state.userLoggedIn = !state.userLoggedIn;
+    },
+  },
+  actions: {
+    async register({ commit }, payload) {
+      const {
+        name, email, password, age, country,
+      } = payload;
+
+      const { user } = await auth
+        .createUserWithEmailAndPassword(email, password);
+
+      await usersCollection.doc(user.uid).set({
+        name, email, age, country,
+      });
+
+      await user.updateProfile({ displayName: name });
+
+      commit('toggleAuth');
+    },
+    async login({ commit }, payload) {
+      const { email, password } = payload;
+
+      await auth.signInWithEmailAndPassword(email, password);
+
+      commit('toggleAuth');
+    },
+    init_login({ commit }) {
+      const user = auth.currentUser;
+      if (user) {
+        commit('toggleAuth');
+      }
+    },
+    async signout({ commit }) {
+      await auth.signOut();
+      commit('toggleAuth');
     },
   },
   getters: {},
