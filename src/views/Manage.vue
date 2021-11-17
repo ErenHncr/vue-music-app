61c303a85dd753a787c0d178450980e3763d6aff
erenhncr <erenhncr@gmail.com>
2021-11-18 01:35:23 +0300

    route guard added for edited songs

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -2,7 +2,10 @@
   <!-- Main Content -->
   <section class="container mx-auto mt-6">
     <div class="md:grid md:grid-cols-3 md:gap-4">
-      <AppUpload ref="upload" />
+      <AppUpload
+        ref="upload"
+        :addSong="addSong"
+      />
       <div class="col-span-2">
         <div class="bg-white rounded border border-gray-200 relative flex flex-col">
           <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
@@ -17,6 +20,8 @@
               :song="song"
               :index="index"
               :updateSong="updateSong"
+              :removeSong="removeSong"
+              :updateUnsavedFlag="updateUnsavedFlag"
             />
           </div>
         </div>
@@ -38,26 +43,47 @@ export default {
     CompositionItem,
   },
   data() {
-    return { songs: [] };
+    return {
+      songs: [],
+      unsavedFlag: false,
+    };
   },
   async created() {
     const snapshot = await songsCollection
       .where('uid', '==', auth?.currentUser?.uid)
       .get();
 
-    snapshot.forEach((doc) => {
-      const song = {
-        docID: doc.id,
-        ...doc.data(),
-      };
-      this.songs.push(song);
-    });
+    snapshot.forEach(this.addSong);
   },
   methods: {
     updateSong(index, values) {
       this.songs[index].modified_name = values?.modified_name;
       this.songs[index].genre = values?.genre;
     },
+    removeSong(index) {
+      this.songs.splice(index, 1);
+    },
+    addSong(doc) {
+      if (typeof doc?.data === 'function') {
+        const song = {
+          docID: doc.id,
+          ...doc.data(),
+        };
+        this.songs.push(song);
+      }
+    },
+    updateUnsavedFlag(value) {
+      this.unsavedFlag = value;
+    },
+  },
+  beforeRouteLeave(to, from, next) {
+    if (!this.unsavedFlag) {
+      next();
+    } else {
+      // eslint-disable-next-line no-alert, no-restricted-globals
+      const leave = confirm('You have unsaved changes. Are you sure you want to leave?');
+      next(leave);
+    }
   },
   // beforeRouteLeave(to, from, next) {
   //   this.$refs.upload.cancelUploads();
