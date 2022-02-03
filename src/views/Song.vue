e6038c3a50996b647e733f475538cf53f5e736de
Eren Hançer <eren.hancer@akinon.com>
2022-02-03 16:36:44 +0300

    add fetch comments and sort functionality

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -22,7 +22,7 @@
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <!-- Comment Count -->
-        <span class="card-title">Comments (15)</span>
+        <span class="card-title">Comments ({{ song?.comment_count}})</span>
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
@@ -57,8 +57,10 @@
         </VeeForm>
         <!-- Sort Comments -->
         <select
+          v-model="sort"
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition
-          duration-500 focus:outline-none focus:border-black rounded">
+          duration-500 focus:outline-none focus:border-black rounded"
+        >
           <option value="1">Latest</option>
           <option value="2">Oldest</option>
         </select>
@@ -67,77 +69,18 @@
   </section>
   <!-- Comments -->
   <ul class="container mx-auto">
-    <li class="p-6 bg-gray-50 border border-gray-200">
+    <li
+      v-for="comment in sortedComments"
+      :key="comment.docID"
+      class="p-6 bg-gray-50 border border-gray-200"
+    >
       <!-- Comment Author -->
       <div class="mb-5">
-        <div class="font-bold">Elaine Dreyfuss</div>
-        <time>5 mins ago</time>
+        <div class="font-bold">{{ comment?.name }}</div>
+        <time>{{ comment?.datePosted }}</time>
       </div>
 
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium der doloremque laudantium.
-      </p>
-    </li>
-    <li class="p-6 bg-gray-50 border border-gray-200">
-      <!-- Comment Author -->
-      <div class="mb-5">
-        <div class="font-bold">Elaine Dreyfuss</div>
-        <time>5 mins ago</time>
-      </div>
-
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium der doloremque laudantium.
-      </p>
-    </li>
-    <li class="p-6 bg-gray-50 border border-gray-200">
-      <!-- Comment Author -->
-      <div class="mb-5">
-        <div class="font-bold">Elaine Dreyfuss</div>
-        <time>5 mins ago</time>
-      </div>
-
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium der doloremque laudantium.
-      </p>
-    </li>
-    <li class="p-6 bg-gray-50 border border-gray-200">
-      <!-- Comment Author -->
-      <div class="mb-5">
-        <div class="font-bold">Elaine Dreyfuss</div>
-        <time>5 mins ago</time>
-      </div>
-
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium der doloremque laudantium.
-      </p>
-    </li>
-    <li class="p-6 bg-gray-50 border border-gray-200">
-      <!-- Comment Author -->
-      <div class="mb-5">
-        <div class="font-bold">Elaine Dreyfuss</div>
-        <time>5 mins ago</time>
-      </div>
-
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium der doloremque laudantium.
-      </p>
-    </li>
-    <li class="p-6 bg-gray-50 border border-gray-200">
-      <!-- Comment Author -->
-      <div class="mb-5">
-        <div class="font-bold">Elaine Dreyfuss</div>
-        <time>5 mins ago</time>
-      </div>
-
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium der doloremque laudantium.
-      </p>
+      <p>{{ comment?.content }}</p>
     </li>
   </ul>
 </template>
@@ -158,16 +101,37 @@ export default {
       comment_show_alert: false,
       comment_alert_variant: 'bg-blue-500',
       comment_alert_message: 'Please wait! Your comment is being submitted',
+      comments: [],
+      sort: '1',
     };
   },
-  computed: { ...mapState(['userLoggedIn']) },
+  computed: {
+    ...mapState(['userLoggedIn']),
+    sortedComments() {
+      return this.comments.slice().sort((a, b) => {
+        // Latest
+        if (this.sort === '1') {
+          return new Date(b?.datePosted) - new Date(a?.datePosted);
+        }
+        // Oldest
+        return new Date(a?.datePosted) - new Date(b?.datePosted);
+      });
+    },
+  },
   async created() {
     const docSnapshot = await songsCollection.doc(this.$route.params.id).get();
     if (!docSnapshot.exists) {
       this.$router.push({ name: 'home' });
       return;
     }
+
+    const { sort } = this.$route.query;
+    if (['1', '2'].includes(sort)) {
+      this.sort = sort;
+    }
+
     this.song = docSnapshot.data();
+    this.getComments();
   },
   methods: {
     // resetForm comes from VeeForm's submit function
@@ -187,11 +151,38 @@ export default {
 
       await commentsCollection.add(comment);
 
+      this.song.comment_count += 1;
+      await songsCollection.doc(this.$route.params.id)
+        .update({ comment_count: this.song.comment_count });
+
+      this.getComments();
+
       this.comment_in_submission = false;
       this.comment_alert_variant = 'bg-green-500';
       this.comment_alert_message = 'Comment added!';
 
       resetForm();
+    },
+    async getComments() {
+      const snapshots = await commentsCollection.where(
+        'sid', '==', this.$route.params.id,
+      ).get();
+
+      this.comments = [];
+
+      snapshots.forEach((doc) => {
+        this.comments.push({
+          docID: doc.id,
+          ...doc.data(),
+        });
+      });
+    },
+  },
+  watch: {
+    sort(newValue) {
+      if (newValue !== this.$route.query.sort) {
+        this.$router.push({ query: { sort: newValue } });
+      }
     },
   },
 };
