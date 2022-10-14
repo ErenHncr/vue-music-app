634e3376c09ea3f2943390826c5b6f8311305daa
erenhncr <erenhncr@gmail.com>
2022-10-14 16:08:23 +0300

    add volume control and profile icon

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -1,15 +1,61 @@
 <template>
   <header class='header' :data-song='Boolean(currentSong?.url)'>
-    <PlaybackControl v-if='!userLoggedIn' />
-    <div class='header__auth'>
-      <BaseButton
-        v-if='userLoggedIn'
-        class='header__btn-signin'
-        color='secondary'
-        @onClick='openAuthModal'
-        >
-        {{ $t('header.signin') }}
-      </BaseButton>
+    <div class='header-wrapper'>
+      <PlaybackControl />
+      <div class="header-controls-container">
+        <div v-if='currentSong?.uid' class='header__volume'>
+          <img
+            class='svg-white header__volume-icon'
+            :src='volumeSVG'
+            width='18'
+            height='18'
+          />
+          <BaseRange
+            type='range'
+            name='volume-control'
+            class="header__volume-control"
+            :value='volume'
+            @change.prevent='onUpdateVolume'
+          />
+        </div>
+        <div class='header__auth'>
+          <BaseButton
+            v-if='userLoggedIn'
+            class='header__btn-signin'
+            color='secondary'
+            @onClick='openAuthModal'
+            >
+            {{ $t('header.signin') }}
+          </BaseButton>
+          <div
+            v-if='!userLoggedIn && currentSong?.uid'
+            class='header__avatar'
+          >
+            <BaseButton
+              class='header__avatar-btn'
+              color='link'
+              @onClick='openAuthModal'
+              >
+              <img
+                class='svg-red-800'
+                :src='avatarSVG'
+                width='24'
+                height='24'
+              />
+            </BaseButton>
+            <div
+              v-if="true"
+              class='header__avatar-dropdown'
+            >
+              <ul>
+                <li>Manage</li>
+                <li>Settings</li>
+                <li>Sign Out</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </header>
   <!-- Auth Modals -->
@@ -70,6 +116,8 @@ import Login from '@/components/partials/Forms/Login.vue';
 import Register from '@/components/partials/Forms/Register.vue';
 import ForgotPassword from '@/components/partials/Forms/ForgotPassword.vue';
 import { PlaybackControl } from '@/components/partials/Playback';
+import volumeSVG from '@/assets/svg/volume.svg';
+import avatarSVG from '@/assets/svg/avatar.svg';
 
 export default {
   name: 'Header',
@@ -79,8 +127,12 @@ export default {
     ForgotPassword,
     PlaybackControl,
   },
+  data() {
+    return { volumeSVG, avatarSVG };
+  },
   methods: {
     ...mapMutations('auth', ['openAuthModal', 'closeAuthModal', 'updateAuthModal']),
+    ...mapMutations('player', ['updateVolume']),
     signout() {
       this.$store.dispatch('signout');
       if (this.$route.meta.requiresAuth) {
@@ -100,12 +152,17 @@ export default {
       const { email } = values;
       this.$store.dispatch('auth/forgotPassword', email, { root: true });
     },
+    onUpdateVolume(e) {
+      const { target: { value } } = e;
+      this.updateVolume(value);
+    },
   },
   computed: {
     ...mapState({
       auth: (state) => state.auth,
       userLoggedIn: (state) => state.auth.userLoggedIn,
       currentSong: (state) => state.player.currentSong,
+      volume: (state) => state.player.volume,
     }),
     currentLocale() {
       return this.$i18n.t('locale');
@@ -126,16 +183,10 @@ export default {
 
 <style lang='scss' scoped>
 .header {
-  height: 55px;
-  display: grid;
-  grid-template-columns: calc(30% - 22px) minmax(40%,900px) calc(30% + 22px);
-  grid-template-rows: 55px;
-  align-items: center;
-  margin: 0 auto;
-  z-index: 1;
+  z-index: 9;
 
   &[data-song='true'] {
-    width: -webkit-fill-available;
+    width: calc(100vw - 260px);
     position: fixed;
     top: 0;
     background-color: rgba(45, 45, 45, .88);
@@ -146,32 +197,72 @@ export default {
   }
 
   &[data-song='false'] {
-    width: 100%;
-    max-width: 1680px;
-    padding-inline-end: 20px;
+    .header__auth {
+      margin-left: auto;
+     }
   }
 
-  &__auth {
-    margin-left: auto;
+  &-wrapper {
+    width: 100%;
+    max-width: 1680px;
+    height: 55px;
+    display: grid;
+    grid-template-columns: calc(30% - 22px) minmax(40%,900px) calc(30% + 22px);
+    grid-template-rows: 55px;
+    align-items: center;
+    margin: 0 auto;
+    z-index: 1;
+  }
+
+  &-controls-container {
+    display: flex;
+    align-items: center;
     grid-column-start: 3;
   }
 
-  &__btn-signin {
-    height: 28px;
-    padding: 6px 12px;
-    border-radius: 6px;
-    margin-left: auto;
-    font-weight: 500;
-    letter-spacing: 0.4px;
+  &__auth {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-inline-end: 20px;
   }
 
-  &__btn-register {
-    font-size: .93rem !important;
-    margin-top: 4rem;
+  &__volume {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2px;
+    padding: 0 10px;
+    margin: 0 auto;
+
+    &-control {
+      width: 70px;
+    }
+
+    &-icon {
+      opacity: 0.4;
+      padding: 2px;
+    }
   }
 
-  &__btn-forgot {
-    font-size: .93rem !important;
+  &__btn {
+    &-signin {
+      height: 28px;
+      padding: 6px 12px;
+      border-radius: 6px;
+      margin-left: auto;
+      font-weight: 500;
+      letter-spacing: 0.4px;
+    }
+
+    &-register {
+      font-size: .93rem !important;
+      margin-top: 4rem;
+    }
+
+    &-forgot {
+      font-size: .93rem !important;
+    }
   }
 }
 .modal {
